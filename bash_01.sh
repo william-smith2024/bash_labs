497ff32e54f2036b94406ff91089b84eb6c8c119
William <williamdsmithjr@gmail.com>
2024-04-30 18:20:45 -0500

    first commit

---
+++ bash_01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Our first bash script.
+#We are going to Echo(print to command line)
+
+echo "Yaaaooooooooooooooooo"
+
+
+
---
+++ bash_02.sh
@@ -0,0 +1,6 @@
+#Read command Allows user to input variable data
+echo "Enter a number"
+read number
+echo $number
+echo "Your number is $number"
+echo "Lets do math"
